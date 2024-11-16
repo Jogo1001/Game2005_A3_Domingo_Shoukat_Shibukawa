cb1bd722c6265aa62f60fe3103febb0102b02093
Jogo1001 <101247726@georgebrown.ca>
2024-11-15 19:49:33 -0500

    adding slingshot draglines

--- GAME2005_A3_Domingo_Shoukat_Shibukawa/Assets/Scripts/pokeball.cs
+++ GAME2005_A3_Domingo_Shoukat_Shibukawa/Assets/Scripts/pokeball.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -73,6 +74,15 @@ public class pokeball : MonoBehaviour
 
 
         }
+
+    }
+    private void UpdateSlingShotLines()
+    {
+        Slingshot_Line_Left.SetPosition(0, Slingshot_left.position);
+        Slingshot_Line_Left.SetPosition(1, Pokeball.transform.position);
+
+        Slingshot_Line_Right.SetPosition(0, Slingshot_right.position);
+        Slingshot_Line_Right.SetPosition(1, Pokeball.transform.position);
 
     }
 }
