3ab28f4b6a1b23abc1ea63024619b3edc7b83b16
Jogo1001 <101247726@georgebrown.ca>
2024-11-15 19:41:25 -0500

    slingshot dragging function

--- GAME2005_A3_Domingo_Shoukat_Shibukawa/Assets/Scripts/pokeball.cs
+++ GAME2005_A3_Domingo_Shoukat_Shibukawa/Assets/Scripts/pokeball.cs
@@ -10,18 +10,51 @@ public class pokeball : MonoBehaviour
     public Transform Slingshot_right;
     public Rigidbody2D Pokeball;
     public float Slingshot_Power = 0f;
-    public float Drag_Distance = 0f;
+    public float Max_Drag_Distance = 0f;
     public LineRenderer Slingshot_Line_Left;
     public LineRenderer Slingshot_Line_Right;
     public Text ScoreText;
+
+
+    private bool IsDragging = false;
+    private Vector2 Drag_Start_Position;
+    private Vector2 Drag_Release_Position;
+    private int score = 0;
     void Start()
     {
+
+
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsDragging)
+        {
+            Vector2 MouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 Displacement = MouseWorldPosition - (Vector2)Pokeball_Rest_Position.position;
+
+            if (Displacement.x > 0) Displacement.x = 0;
+            if(Displacement.magnitude > Max_Drag_Distance)
+            {
+                Displacement = Displacement.normalized * Max_Drag_Distance;
+            }
+            Pokeball.transform.position = (Vector2)Pokeball_Rest_Position.position + Displacement;
+
+
+
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(!IsDragging)
+        {
+            IsDragging = true;
+            Pokeball.isKinematic = true;
+        }
     }
 }
